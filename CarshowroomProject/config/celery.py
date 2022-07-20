c8a644427d53eee57e9acb8e8b9e8275d819aa41
ChainHokes <k.nazar2003@gmail.com>
2022-07-20 18:09:51 +0300

    linter fix

--- CarshowroomProject/config/celery.py
+++ CarshowroomProject/config/celery.py
@@ -2,20 +2,23 @@ import os
 from celery import Celery
 from celery.schedules import crontab
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
+
 @app.task(bind=True)
 def debug_task(self):
     print(f'Request: {self.request!r}')
 
+
 app.conf.beat_schedule = {
     'buy_suppliers_cars': {
         'task': 'src.carshowroom.tasks.buy_suppliers_cars',
-        'schedule': crontab(minute = '*/10'),
+        'schedule': crontab(minute='*/10'),
     },
     'check_suppliers_benefit': {
         'task': 'src.carshowroom.tasks.check_supplier_benefit',
@@ -23,6 +26,6 @@ app.conf.beat_schedule = {
     },
     'accept_offer': {
         'task': 'src.carshowroom.tasks.accept_offer',
-        'schedule': crontab(minute = '*/10'),
+        'schedule': crontab(minute='*/10'),
     }
 }
